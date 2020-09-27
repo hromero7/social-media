09404ba04467d5ae253087c5202551160cc620f3
hrome <58020216+hromero7@users.noreply.github.com>
2020-09-27 16:23:58 -0700

    setting up back end server and database

---
+++ atlas/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+require("dotenv").config();
+const app = express();
+
+app.use(cors());
+
+
+// const uri = process.env.ATLAS_URI;
+
+mongoose.connect(process.env.ATLAS_URI || "mongodb://localhost/socialapp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
+
+
+mongoose.connection.on("connected", () => {
+    console.log("mongoose is connected");
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+//Define API Routes here
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
+  });
+  
