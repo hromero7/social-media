855feee4a31a179741b8ffc2abddc803d5e1c15d
asedlack <asedlackcode@gmail.com>
2020-09-29 20:30:55 -0700

    login page layout

--- atlas/client/src/App.js
+++ atlas/client/src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import Login from "./pages/Login";
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+      <Route exact path= "/" component={Login}/>
+      </div>
+    </Router>
+    
   );
 }
 
