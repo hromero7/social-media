b5c4cb5b0116a5c24ff4de5668984a5c488588db
hrome <58020216+hromero7@users.noreply.github.com>
2020-10-19 19:01:26 -0700

    setting up bcrypt

--- atlas/models/user.js
+++ atlas/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const Schema = mongoose.Schema;
 
@@ -26,6 +27,31 @@ password: {
     required: true
 }
 });
+
+UserSchema.pre("save", function(next) {
+    if(!this.isModified("password")) {
+        return next();
+    }
+    bcrypt.hash(this.password, 10, (err, hashPassword) => {
+        if (err) {
+            return next(err);
+        }
+        this.password = hashPassword;
+        next();
+    });
+});
+
+UserSchema.methods.comparePassword = function(password, cb) {
+    bcrypt.compare(password, this.password, (err, isMatch) => {
+        if (err) {
+            return cb(err);
+        } else if (!isMatch) {
+            return cb(null, isMatch); 
+        } else {
+            return cb(null, this);
+        }
+    })
+}
 
 const User = mongoose.model("User", UserSchema);
 
